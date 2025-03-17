cf53ae64e89098c35897de19fd4594245ca80c6a
luis <gutierrez.luis@icloud.com>
2025-03-17 15:09:18 -0700

    Add files via upload

--- list_with_loop.py
+++ list_with_loop.py
@@ -1,3 +1,7 @@
 #[expression for tempvar in originaliterable]
 x = [nametag.capitalize() for nametag in ["granade", "bazooka"]]
 print(x)
+y = [i.upper() for i in ["granade"]]
+print(y)
+z = [i.lower() for i in ["ROCKET LAUNCHER"]]
+print(z)
