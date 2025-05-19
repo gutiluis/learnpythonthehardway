a2cb4c5b56327d755f5ae0f44ee1f968d242822a
luis <gutierrez.luis@icloud.com>
2025-05-18 23:43:29 -0700

    Add files via upload

--- ex31.py
+++ ex31.py
@@ -1,7 +1,7 @@
-print("""You enter a dark room with three doors for now.
-Do you go through door #1, door #2, or door #3""")
+print("""You enter a dark room with two doors.
+Do you go through door #1 or door #2?""")
 
-door = input(">  ")
+door = input("> ")
 
 if door == "1":
     print("There's a giant bear here eating a cheese cake.")
@@ -9,8 +9,8 @@ if door == "1":
     print("1. Take the cake.")
     print("2. Scream at the bear.")
 
-    bear = input(">  ")
-    # if inside if
+    bear = input("> ")
+
     if bear == "1":
         print("The bear eats your face off. Good job!")
     elif bear == "2":
@@ -25,26 +25,14 @@ elif door == "2":
     print("2. Yellow jacket clothespins.")
     print("3. Understanding revolvers yelling melodies.")
 
-    insanity = input(">  ")
-    # if inside if
-    if insanity == "1" or instanity == "2":
-        print("Your body survive powered by a mind of jello.")
+    insanity = input("> ")
+
+    if insanity == "1" or insanity == "2":
+        print("Your body survives powered by a mind of jello.")
         print("Good job!")
     else:
         print("The insanity rots your eyes into a pool of muck.")
         print("Good job!")
-elif door == "3":
-    print("Kill everybody.")
-    print("Choose a weapon:")
-    print("1. Bazooka")
-    print("2. Granade")
 
-    weapon = input("> ")
-    if weapon == "1":
-        print("Kill all the monkeys with the bazooka.")
-    elif weapon == "2":
-        print("Kill all the monkeys with the granade.")
-    else:
-        print("The monkeys will kill you.")
 else:
     print("You stumble around and fall on a knife and die. Good job!")
